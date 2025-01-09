3dc744ca1ba6e3a2dbf6739c8cca46df1a2363ab
MikaAmann <93271414+MikaAmann@users.noreply.github.com>
2025-01-09 10:55:42 +0100

    Update distractionDetection.py

--- HeadPoseEstimation/distractionDetection.py
+++ HeadPoseEstimation/distractionDetection.py
@@ -27,25 +27,28 @@ def analyze_image_with_yolo(image_path):
 
     # Bild mit YOLO verarbeiten
     results = model(image_path)
+    try:
+        # Koordinaten der Gesichtspunkte extrahieren
+        keypoints = results[0].keypoints.xy.numpy()  # Konvertiere zu NumPy-Array
+        nose = np.array([keypoints[0][0][0], keypoints[0][0][1]])  # x, y der Nase
+        left_eye = np.array([keypoints[0][1][0], keypoints[0][1][1]])  # x, y des linken Auges
+        right_eye = np.array([keypoints[0][2][0], keypoints[0][2][1]])  # x, y des rechten Auges
 
-    # Koordinaten der Gesichtspunkte extrahieren
-    keypoints = results[0].keypoints.xy.numpy()  # Konvertiere zu NumPy-Array
-    nose = np.array([keypoints[0][0][0], keypoints[0][0][1]])  # x, y der Nase
-    left_eye = np.array([keypoints[0][1][0], keypoints[0][1][1]])  # x, y des linken Auges
-    right_eye = np.array([keypoints[0][2][0], keypoints[0][2][1]])  # x, y des rechten Auges
+        # Mittelpunkt der Augen berechnen
+        eye_center_x = (left_eye[0] + right_eye[0]) / 2
+        eye_center_y = (left_eye[1] + right_eye[1]) / 2
 
-    # Mittelpunkt der Augen berechnen
-    eye_center_x = (left_eye[0] + right_eye[0]) / 2
-    eye_center_y = (left_eye[1] + right_eye[1]) / 2
+        # Differenzen berechnen
+        dx = nose[0] - eye_center_x
+        dy = nose[1] - eye_center_y
 
-    # Differenzen berechnen
-    dx = nose[0] - eye_center_x
-    dy = nose[1] - eye_center_y
+        # Winkel in Grad berechnen
+        yaw_angle = (math.atan2(dy, dx) * (180 / math.pi)) - 90
 
-    # Winkel in Grad berechnen
-    yaw_angle = (math.atan2(dy, dx) * (180 / math.pi)) - 90
-
-    return yaw_angle
+        return yaw_angle
+    except:
+        return None
+        
 
 def setup():
     #Setup-Funktion zur Aufnahme der Links- und Rechts-Winkel.
@@ -57,6 +60,9 @@ def setup():
         print("Fehler beim Aufnehmen des linken Bildes.")
         return None, None
     left_angle = analyze_image_with_yolo(left_image_path)
+    
+    if left_angle == None:
+        return None, None
 
     print("Bitte nach rechts schauen und Enter drücken.")
     input("Drücken Sie Enter, nachdem Sie bereit sind...")
@@ -77,11 +83,13 @@ def monitor_angles(left_angle, right_angle):
             print("Fehler beim Aufnehmen des aktuellen Bildes.")
             continue
         current_angle = analyze_image_with_yolo(current_image_path)
-
-        if current_angle < left_angle:
-            print("Warnung: Person schaut weiter nach links als im Setup angegeben!")
+        
+        if current_angle == None:
+            print("Warnung: Blickwinkel konnte nicht erfasst werden!")
         elif current_angle > right_angle:
             print("Warnung: Person schaut weiter nach rechts als im Setup angegeben!")
+        elif current_angle < left_angle:
+            print("Warnung: Person schaut weiter nach links als im Setup angegeben!")
         else:
             print("Blickwinkel ist innerhalb des zulässigen Bereichs.")
 
