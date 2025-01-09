df025053382136f14cc69eb629949c91800f68e3
Patric Bier <patric-bier@gmx.de>
2025-01-09 10:40:17 +0100

    Update wisper, Command recognition
    Command recognition
    improved decoding settings

--- TTS_and_STT/wisper_STT.py
+++ TTS_and_STT/wisper_STT.py
@@ -1,10 +1,19 @@
 import whisper
-
+from enum import Enum
 
 # Use push to Talk 'l' for voce request
 
 
 model = whisper.load_model("tiny")
+
+
+# Comand Listen 
+qNA_comands: list[str] = ["starte q and a", "q and a starte q and a", "abfrage starten"]
+free_learning_comands: list[str] = ["starte freies lernen", "freises lernen starten"]
+start_comands: list[str] = ["starte"]
+bende_comands: list[str] = ["ende", "beende"]
+
+comads= Enum ('comnad',[('starten',1),('benden',2),('qna',3)]) 
 
 # wisper 
 def audioToString(path:str) ->str:
@@ -17,10 +26,21 @@ def audioToString(path:str) ->str:
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
     # detect the spoken language
-    _, probs = model.detect_language(mel)
-  
+    #_, probs = model.detect_language(mel)
+
     # decode the audio
-    options = whisper.DecodingOptions()
+    options = whisper.DecodingOptions(
+        language="de",
+        fp16=False,           # Verhindert die Nutzung von FP16 für genauere Berechnungen (wenn notwendig)
+        beam_size=5,          # Höhere Beamgröße führt zu besseren Ergebnissen, aber langsamerer Verarbeitung
+        best_of=5,            # Versucht 'best_of' der besten Ergebnisse (empfohlen bei leistungsstarken GPUs)
+        temperature=0.0,      # Setzt die Temperatur auf 0 für deterministische Ergebnisse
+        patience=1.0,         # Erhöht das Geduld-Verhältnis, um längere Zeit für das Decoding zu lassen
+        length_penalty=1.0,   # Verhindert unnötig lange Ausgaben (kann verringert werden, wenn du lange Transkripte erwartest)
+        suppress_blank=False, # Verhindert die Unterdrückung von "leeren" (Stille)-Tokens, wenn sie nicht notwendig sind
+    )
+
+
     result = whisper.decode(model, mel, options)
     fileToWrite= "OUTPUT.txt"
     print(result.text)
@@ -29,3 +49,10 @@ def audioToString(path:str) ->str:
    
     return result.text
 
+def is_a_comand(text:str, wordlist:list[str]) ->bool:
+    value: bool = False
+    for i in wordlist:
+        if text.__contains__(i):# reconice Comand
+            value = True
+    
+    return value
